3b79295ddf62361cb661dfdc19e08742cbbb03d4
DimaSavchenko <dimasauchanka@gmail.com>
2018-04-20 18:31:05 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const style = {
+    fontWeight: '100',
+    textAlign: 'center',
+    color: '#777'
+};
+
+ReactDOM.render(
+    <div style={style}>
+        <h1>Интернет-киоск с англоязычными газетами и журналами в Беларуси.</h1>
+        <p>coming soon...</p>
+    </div>,
+    document.getElementById('root')
+);
+
+
